bff6f7029bb3c0c84fd6ea8f5875e83da668e743
adityapratama1999 <apratama25064@gmail.com>
2026-04-04 13:23:14 +0000

    Refactor dashboard.py: Change order resampling frequency to 'ME' and remove RFM analysis section

--- Dasboard/dashboard.py
+++ Dasboard/dashboard.py
@@ -9,7 +9,7 @@ sns.set(style='dark')
 
 def create_daily_orders_df(df):
     df['date'] = pd.to_datetime(df['date'])
-    orders_df = df.resample('M', on='date').sum()
+    orders_df = df.resample('ME', on='date').sum()
     return orders_df
 
 def create_sum_casual_user_df(df):
@@ -154,44 +154,3 @@ ax[1].ticklabel_format(style='plain', axis='y')
 
 st.pyplot(fig)
 
-# RFM Analysis
-st.subheader("Best Customer Based on RFM Parameters (day)")
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    avg_recency = round(rfm_df.recency.mean(), 1)
-    st.metric("Average Recency (days)", value=avg_recency)
-
-with col2:
-    avg_frequency = round(rfm_df.frequency.mean(), 2)
-    st.metric("Average Frequency", value=avg_frequency)
-
-with col3:
-    avg_frequency = format_currency(rfm_df.monetary.mean(), "AUD", locale='es_CO')
-    st.metric("Average Monetary", value=avg_frequency)
-
-fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(35, 15))
-colors = ["#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9"]
-
-sns.barplot(y="recency", x="day", data=rfm_df.sort_values(by="recency", ascending=True).head(5), palette=colors, hue="day", legend=False, ax=ax[0])
-ax[0].set_ylabel(None)
-ax[0].set_xlabel(None)
-ax[0].set_title("By Recency (days)", loc="center", fontsize=50)
-ax[0].tick_params(axis='y', labelsize=25)
-ax[0].tick_params(axis ='x', labelsize=30, rotation=45)
-
-sns.barplot(y="frequency", x="day", data=rfm_df.sort_values(by="frequency", ascending=False).head(5), palette=colors, hue="day", legend=False, ax=ax[1])
-ax[1].set_ylabel(None)
-ax[1].set_xlabel(None)
-ax[1].set_title("By Frequency", loc="center", fontsize=50)
-ax[1].tick_params(axis='y', labelsize=25)
-ax[1].tick_params(axis='x', labelsize=30, rotation=45)
-
-sns.barplot(y="monetary", x="day", data=rfm_df.sort_values(by="monetary", ascending=False).head(5), palette=colors, hue="day", legend=False, ax=ax[2])
-ax[2].set_ylabel(None)
-ax[2].set_xlabel(None)
-ax[2].set_title("By Monetary", loc="center", fontsize=50)
-ax[2].tick_params(axis='y', labelsize=25)
-ax[2].tick_params(axis='x', labelsize=30, rotation=45)
-
-st.pyplot(fig)
